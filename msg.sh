c10dce30e24302578d2f43faf7f32b1fbc814b0f
ark <ark@ark-server.com>
2015-11-05 17:29:52 +0100

    added scripts

---
+++ msg.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/home/steam/scripts/rcon -P<password> -a127.0.0.1 -p<port> ServerChat $1
+
---
+++ update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/home/steam/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steam/ark-server +app_update "376030 validate" +quit
+
